d4658ae035dc38e7504ddb8b372ef38356408ed8
wingatec <carmelitawingate@gmail.com>
2014-06-26 11:03:09 -0400

    created a Hello World application
    created a hello world application with read and write lines

---
+++ HelloWorld/HelloWorld/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HelloWorld
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string inputReadLine;
+            Console.WriteLine("Hello world!");
+            inputReadLine = Console.ReadLine();
+            Console.WriteLine(inputReadLine);
+            Console.WriteLine("press key");
+            Console.ReadLine();
+
+
+
+        }
+
+    }
+}
